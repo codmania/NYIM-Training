02000abbef26412ea78f10755c71f8140108ceb4
zelig <viktor.tron@gmail.com>
2014-04-05 21:22:14 +0700

    add option to deactivate locations - add active boolean column to locations table in new migration - add active scope to location model - active scope restriction via collection_scope to index action in LocationsController - add active column to list of displayed columns for admin list action in controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,11 @@ class LocationsController < ApplicationController
 
   resourceful_actions :defaults, :list => :index
 
-  display_options :created_at, :name, :venue_link, :assets, :view, :edit,
-                  :only => [:list, :create, :update]
+  collection_scope :index do |scope|
+    scope.active
+  end
+
+  display_options :created_at, :name, :active, :venue_link, :assets, :view, :edit,
+    :only => [:list, :create, :update]
 
 end
