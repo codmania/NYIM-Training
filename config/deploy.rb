08716a75279c9732a1374c5265ba206fc66b2398
zelig <viktor.tron@gmail.com>
2015-01-14 18:43:28 +0000

    removing cap deploy parts for  delayed_job service for svc daemontools - obsolete

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,8 +35,6 @@ set :normalize_asset_timestamps, false
 
 # tasks
 set(:system_files_path) { "#{release_path}/config/deploy/#{stage}" }
-set(:delayed_job_service) { "#{system_files_path}/delayed_job_service" }
-set(:delayed_job_path) { "#{service_root}/#{application}_#{rails_env}_delayed_job" }
 set(:crontab_path) { "#{system_files_path}/crontab" }
 
 # shared tasks
@@ -60,41 +58,3 @@ end
 after "deploy:start", "shared:legacy"
 after "deploy:restart", "shared:legacy"
 
-namespace :delayed_job do
-  desc "Shut off supervise"
-  task :off, :roles => :app do
-    delayed_job.stop
-    sudo "killgrep 'svscan #{service_root}'"
-    sudo "killgrep supervise"
-    sudo "killgrep script/delayed_job"
-  end
-  desc "Switch on supervise"
-  task :on, :roles => :app do
-    sudo "ps aux|grep -v grep|grep -q 'svscan #{service_root}'|| (svscan #{service_root}&) && sleep 5"
-  end
-  desc "Start delayed job"
-  task :start, :roles => :app do
-    delayed_job.stop
-    sudo "ln -sf #{delayed_job_service} #{delayed_job_path}"
-    #sudo "svc -u #{delayed_job_path}"
-  end
-  desc "Stop delayed job"
-  task :stop, :roles => :app do
-    #http://cr.yp.to/daemontools/faq/create.html#remove
-    run "cd -L #{delayed_job_path}/ && sudo rm -rf #{delayed_job_path} && sudo svc -dx . log || echo 'already off'"
-  end
-  task :relaunch, :roles => :app do
-    delayed_job.stop
-    run "sudo mkdir -p #{service_root}"
-    run "mkdir -p #{shared_path}/log/delayed_job_service"
-    run "ln -sf #{shared_path}/log/delayed_job_service #{delayed_job_service}/log/main"
-    run "ln -sf delayed_job_service/current #{shared_path}/log/delayed_job_service.log"
-    run "chmod +x #{delayed_job_service}/run"
-    run "chmod +x #{delayed_job_service}/log/run"
-    delayed_job.start
-  end
-end
-
-after "deploy:start", "delayed_job:relaunch"
-after "deploy:stop", "delayed_job:stop"
-after "deploy:restart", "delayed_job:relaunch"
