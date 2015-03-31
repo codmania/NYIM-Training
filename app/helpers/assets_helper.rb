3e9aa9ef43bbfaed83de88f0d835c102dc77a95c
Stanislav Katkov <lunaticman@gmail.com>
2015-04-01 04:02:23 +0700

    fix for last commit

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -32,7 +32,7 @@ module AssetsHelper
     formtastic_button(form)
     w.rawtext "<script type=\"text/javascript\"> if('#asset_content'){
                 CKEDITOR.replace('asset_content', {});
-                $('label.label').remove();
+                $('.asset_view_box label.label').remove();
               } </script>"
   end
 
