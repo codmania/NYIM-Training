f66330f06ba9730a214d20e4f1f4744dc44e6b99
Stanislav Katkov <lunaticman@gmail.com>
2015-04-03 14:49:07 +0700

    send certificates in a same time feedback forms are sent

--- lib/nyim_jobs/send_certificates.rb
+++ lib/nyim_jobs/send_certificates.rb
@@ -4,7 +4,7 @@ class NyimJobs::SendCertificates < NyimJobs::Base
   def perform
     transaction do
       if Site.site(:notify_when_certificate_available)
-        in_batches Signup.certificates_to_be_mailed do |signup|
+        in_batches Signup.confirmed.attendance.inthelasthour.reject(&:blank?) do |signup|
 
           Mailers::UserMailer.certificate({},{ :student => signup.student, :signup => signup })
           signup.update_attribute :certificate_mailed_on, Time.now
